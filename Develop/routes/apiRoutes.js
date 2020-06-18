d9bbc8ea103f4ecc34c19553cb34e90055c94d72
Ha Phan <haphan64@outlook.com>
2020-06-17 23:23:43 -0700

    html routes

---
+++ Develop/routes/apiRoutes.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+
+router.get("/notes", (req, res) => {
+
+
+
+});
+
+router.post("/notes", (req, res) => {
+
+    
+
+});
+
+router.delete("/notes/:id", (req, res) => {
+
+    
+
+});
+
+module.exports = router;
